8237cf3fb442378971199400d6aa16e136927737
Mohammad Junaid Ali#EXT# <Mohammad.Ali@triona.no>
2024-02-14 17:59:32 +0100

    Fail 1

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
 import * as THREE from 'three';
-import * as THREEx from './node_modules/@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js';
-
-
+import 'three/examples/js/controls/DeviceOrientationControls.js'; // Import DeviceOrientationControls separately
+import * as THREEx from 'ar.js';
 
 const canvas = document.getElementById('canvas1');
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
-const renderer = new THREE.WebGLRenderer({canvas: canvas});
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
-const arjs = new THREEx.LocationBased(scene, camera);
-const cam = new THREEx.WebcamRenderer(renderer);
+const arToolkitSource = new THREEx.ArToolkitSource({
+    sourceType: 'webcam',
+});
+
+const arToolkitContext = new THREEx.ArToolkitContext({
+    cameraParametersUrl: 'data/camera_para.dat',
+    detectionMode: 'mono',
+});
+
+arToolkitContext.init(() => {
+    camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
+});
+
+const markerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, {
+    type: 'pattern',
+    patternUrl: 'data/pattern-marker.patt',
+    changeMatrixMode: 'cameraTransformMatrix',
+});
 
 const geom = new THREE.BoxGeometry(20, 20, 20);
-const mtl = new THREE.MeshBasicMaterial({color: 0xff0000});
+const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const box = new THREE.Mesh(geom, mtl);
 
-// Create the device orientation tracker
-const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
+scene.add(box);
 
-// Change this to a location close to you (e.g. 0.001 degrees of latitude north of you)
-arjs.add(box, 10.759166, 59.908562);
+markerControls.addEventListener('markerFound', () => {
+    box.visible = true;
+});
 
-arjs.startGps();
+markerControls.addEventListener('markerLost', () => {
+    box.visible = false;
+});
 
-requestAnimationFrame(render);
-
-function render() {
-    if(canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
-        renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-        const aspect = canvas.clientWidth/canvas.clientHeight;
-        camera.aspect = aspect;
-        camera.updateProjectionMatrix();
-    }
-
-    // Update the scene using the latest sensor readings
-    deviceOrientationControls.update();
-
-    cam.update();
+function animate() {
+    requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    requestAnimationFrame(render);
 }
+
+arToolkitSource.init(() => {
+    setTimeout(() => {
+        canvas.style.display = 'block';
+        animate();
+    }, 200);
+});
